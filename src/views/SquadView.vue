c9d504443ef8253bc0aab5a35aec08b0ec9d165a
JRLee <49389212+JReal2@users.noreply.github.com>
2023-12-10 02:20:32 +0900

    style: 스쿼드 페이지 수정

--- src/views/SquadView.vue
+++ src/views/SquadView.vue
@@ -1,23 +1,150 @@
 <template>
   <v-app class="app">
-    <v-list-item
-      v-for="(item,idx) in Array.from({length: 10})"
-      :key="idx"
+<!--    <v-list-item-->
+<!--      v-for="(item,idx) in Array.from({length: 10})"-->
+<!--      :key="idx"-->
+<!--    >-->
+<!--      <v-img-->
+<!--        :src="!players.has(idx) ? addImageUrl : players.get(idx).image"-->
+<!--        width="50px"-->
+<!--        height="50px"-->
+<!--        @click="()=>onClickPlayerAddButton(idx)"-->
+<!--      />-->
+<!--    </v-list-item>-->
+    <v-row />
+    <v-row />
+    <v-row />
+    <v-row
+      class="justify-center"
     >
-      <v-img
-        :src="!players.has(idx) ? addImageUrl : players.get(idx).image"
-        width="50px"
-        height="50px"
-        @click="()=>onClickPlayerAddButton(idx)"
-      />
-    </v-list-item>
+      <v-spacer />
+      <v-col
+        cols="1"
+      >
+        <v-img
+          class="mx-auto"
+          :src="!players.has(8) ? addImageUrl : players.get(8).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(8)"
+        />
+      </v-col>
+      <v-spacer />
+    </v-row>
+    <v-row
+      class="justify-center"
+    >
+      <v-spacer />
+      <v-col cols="3">
+        <v-img
+          class="ml-16"
+          :src="!players.has(7) ? addImageUrl : players.get(7).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(7)"
+        />
+      </v-col>
+      <v-col cols="5">
+        <v-img
+          :src="!players.has(9) ? addImageUrl : players.get(9).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(9)"
+        />
+      </v-col>
+    </v-row>
+    <v-row />
+    <v-row />
+    <v-row
+      class="justify-center"
+    >
+      <v-col cols="1">
+        <v-img
+          class="mx-auto"
+          :src="!players.has(6) ? addImageUrl : players.get(6).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(6)"
+        />
+      </v-col>
+      <v-col cols="1">
+        <v-img
+          class="mx-auto"
+          :src="!players.has(4) ? addImageUrl : players.get(4).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(4)"
+        />
+      </v-col>
+    </v-row>
+    <v-row />
+    <v-row
+      class="justify-center"
+    >
+      <v-col
+        cols="1"
+        style="padding-right:0"
+      >
+        <v-img
+          class="mx-13"
+          :src="!players.has(5) ? addImageUrl : players.get(5).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(5)"
+        />
+      </v-col>
+      <v-col cols="1">
+        <v-img
+          class="mx-auto"
+          :src="!players.has(1) ? addImageUrl : players.get(1).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(1)"
+        />
+      </v-col>
+      <v-col cols="1">
+        <v-img
+          class="mx-5"
+          :src="!players.has(3) ? addImageUrl : players.get(3).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(3)"
+        />
+      </v-col>
+    </v-row>
+    <v-row />
+    <v-row
+      class="justify-center"
+    >
+      <v-col cols="3">
+        <v-img
+          :src="manager === null ? addImageUrl : manager.image"
+          width="50px"
+          height="50px"
+          @click="onClickManagerAdd"
+        />
+      </v-col>
+      <v-col cols="3">
+        <v-img
+          class="mx-auto"
+          :src="!players.has(2) ? addImageUrl : players.get(2).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(2)"
+        />
+      </v-col>
+      <v-col cols="3">
+        <v-img
+          class="mx-16"
+          :src="!players.has(10) ? addImageUrl : players.get(10).image"
+          width="50px"
+          height="50px"
+          @click="()=>onClickPlayerAddButton(10)"
+        />
+      </v-col>
+    </v-row>
 
-    <v-img
-      :src="manager === null ? addImageUrl : manager.image"
-      width="50px"
-      height="50px"
-      @click="onClickManagerAdd"
-    />
+
 
     <PlayerSearch
       :show-dialog="showPlayerPopup"
@@ -38,7 +165,7 @@
       class="save-btn"
       @click="onClickSaveButton"
     >
-      저장 하기
+      Save
     </v-btn>
   </v-app>
 </template>
@@ -63,7 +190,7 @@ const onClickPlayerAddButton = (positionIndex) => {
 const onClickPlayerPopupClose = () => showPlayerPopup.value = !showPlayerPopup.value;
 
 const onClickManagerAdd = () => (showManagerPopup.value = !showManagerPopup.value);
-const onClickSaveButton = () => alert('저장 되었습니다'); //TODO API 연동
+const onClickSaveButton = () => alert('Squad Saved'); //TODO API 연동
 
 const setManager = selectedManager => manager.value = selectedManager;
 const setPlayer = (position, selectedPlayer) => players.value.set(position, selectedPlayer);
@@ -84,5 +211,12 @@ const searchPlayer = ({ clubId, playerName}) => {
   width: 7%;
   left: 85%;
   bottom: 20%;
+}
+
+.v-row {
+  height:50px;
+}
+.v-col {
+
 }
 </style>
